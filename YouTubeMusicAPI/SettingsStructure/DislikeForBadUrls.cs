88b1168ef5681930e28e7e1862cadf9d15303a03
dmusial98 <dmusial98@gmail.com>
2025-05-10 12:47:17 +0200

    delete unused code

--- YouTubeMusicAPI/SettingsStructure/DislikeForBadUrls.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace YouTubeMusicAPI.SettingsStructure
-{
-    public class DislikeForBadUrls
-    {
-        public bool dislikeForBadUrls { get; set; } = false;
-        public string badUrlsFileName { get; set; } = String.Empty;
-    }
-}
